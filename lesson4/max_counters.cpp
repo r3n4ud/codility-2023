bb042a6db6f69088f78e30cd0d68702d9d1f6e2b
Renaud AUBIN <root@renaud.io>
2023-09-04 13:54:32 +0200

    Add Lesson 4 first draft of solutions

--- lesson4/max_counters.cpp
+++ lesson4/max_counters.cpp
@@ -1,12 +1,33 @@
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 
+#include <algorithm>
 #include <vector>
 
-std::vector<int> solution(int N, std::vector<int>& A) { return {}; }
+std::vector<int> solution(int N, std::vector<int>& A) {
+  std::vector<int> result(N, 0);
+  int max = 0;
+
+  int max_counters_ops = std::count(A.begin(), A.end(), N + 1);
+  if (max_counters_ops > 1) {
+  }
+
+  for (auto item : A) {
+    if (item == N + 1) {
+      std::fill(result.begin(), result.end(), max);
+    } else {
+      int val = result.at(item - 1) + 1;
+      result.at(item - 1) = val;
+      if (val > max) max = val;
+    }
+  }
+
+  return result;
+}
 
 TEST_CASE("Lesson 4 Counting Elements / MaxCounter", "[counting-elements]") {
   std::vector<std::vector<int>> data{
-      {},
+      {3, 4, 4, 6, 1, 4, 4},
   };
-  REQUIRE(solution(0, data.at(0)) == std::vector<int>{});
+  REQUIRE(solution(5, data.at(0)) == std::vector<int>{3, 2, 2, 4, 2});
 }
--- lesson4/missing_integer.cpp
+++ lesson4/missing_integer.cpp
@@ -1,12 +1,66 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <iostream>
+#include <numeric>
+#include <random>
+
+#include <algorithm>
+#include <set>
 #include <vector>
 
-int solution(std::vector<int>& A) { return 0; }
+int solution(std::vector<int>& A) {
+  int result = 1;
+
+  std::set<int> s(std::begin(A), std::end(A));
+  auto first_pos = std::find_if(s.begin(), s.end(), [](int c) { return c > 0; });
+  if (first_pos != s.end()) {
+    s.erase(s.begin(), first_pos);
+  }
+
+  if (!s.empty()) {
+    if (s.size() == 1) {
+      if (*s.begin() == 1) result = 2;
+    } else {
+      if (*s.begin() <= 1) {
+        auto it =
+            std::adjacent_find(s.begin(), s.end(), [](auto l, auto r) { return (l + 1 != r); });
+        if (it == s.end()) {
+          int last_val = *s.rbegin();
+          if (last_val > 0) result = last_val + 1;
+        } else {
+          if (*it > 0) {
+            result = *it + 1;
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
 
 TEST_CASE("Lesson 4 Counting Elements / MissingInteger", "[counting-elements]") {
-  std::vector<std::vector<int>> data{
-      {},
-  };
-  REQUIRE(solution(data.at(0)) == -1);
+  std::vector<std::vector<int>> data{{1, 3, 6, 4, 1, 2}, {1, 2, 3},          {-1, -3}, {1}, {2},
+                                     {4, 5, 6, 2},       {-1000000, 1000000}};
+
+  auto rng = std::default_random_engine{};
+
+  std::vector<int> data_large(200);
+  std::iota(data_large.begin(), data_large.end(), 0);
+  data_large.erase(std::find(data_large.begin(), data_large.end(), 101));
+  std::shuffle(std::begin(data_large), std::end(data_large), rng);
+
+  std::vector<int> data_xl(10005);
+  std::iota(data_xl.begin(), data_xl.end(), -4000);
+  data_xl.erase(std::find(data_xl.begin(), data_xl.end(), 111));
+  std::shuffle(std::begin(data_xl), std::end(data_xl), rng);
+
+  REQUIRE(solution(data.at(0)) == 5);
+  REQUIRE(solution(data.at(1)) == 4);
+  REQUIRE(solution(data.at(2)) == 1);
+  REQUIRE(solution(data.at(3)) == 2);
+  REQUIRE(solution(data.at(4)) == 1);
+  REQUIRE(solution(data.at(5)) == 1);
+  REQUIRE(solution(data.at(6)) == 1);
+  REQUIRE(solution(data_large) == 101);
+  REQUIRE(solution(data_xl) == 111);
 }
